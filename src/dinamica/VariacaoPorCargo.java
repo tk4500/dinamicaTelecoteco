2051a1fc5016bec2d7d9ee5df67007b888678935
Tarcísio Luiz Pereira Bogo <131721143+tk4500@users.noreply.github.com>
2024-08-08 15:23:02 -0300

    Update VariacaoPorCargo.java

--- src/dinamica/VariacaoPorCargo.java
+++ src/dinamica/VariacaoPorCargo.java
@@ -1,23 +1,22 @@
 package dinamica;
 
 import java.math.BigDecimal;
+import java.util.Comparator;
 import java.util.List;
 
 public class VariacaoPorCargo {
 
 	public static void main(String[] args) {
 		List<Funcionario> funcionarios = CargaDados.getListaFuncionarios();
+		BigDecimal menor = menorSalarioCargo(DepartamentoEnum.VENDAS, funcionarios);
+		System.out.println(menor);
 	}
 	
-	public void name() {
-		
+	public static BigDecimal menorSalarioCargo(DepartamentoEnum cargo, List<Funcionario> funcionarios) {
+		return getFuncionarioCargo(cargo, funcionarios).stream().map(f -> f.getSalario()).min(Comparator.naturalOrder()).orElse(BigDecimal.valueOf(0));
 	}
 	
-	public BigDecimal menorSalarioCargo(DepartamentoEnum cargo, List<Funcionario> funcionarios) {
-		
-	}
-	
-	public List<Funcionario> getFuncionarioCargo(DepartamentoEnum cargo, List<Funcionario> funcionarios){
+	public static List<Funcionario> getFuncionarioCargo(DepartamentoEnum cargo, List<Funcionario> funcionarios){
 		return funcionarios.stream().filter(f->f.getCargo().equals(cargo)).toList();
 		
 	}
