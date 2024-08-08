94a1fedf5a43a5bf6cded858903ab5679c0c9443
Guilherme Lessa <guilessa.irmd@gmail.com>
2024-08-08 14:31:46 -0300

    Create Guilherme.java

---
+++ src/dinamica/Guilherme.java
@@ -0,0 +1,16 @@
+package dinamica;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Guilherme {
+	public static void main(String[] args) {
+		CargaDados cargaDados = new CargaDados();
+		cargaDados .getListaFuncionarios();
+		
+		
+		
+	}
+
+}
